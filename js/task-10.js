293c2abb82fe7fe6b431e135d8fe999722f7b66c
DawidKrupski <dawid.krupski323@gmail.com>
2022-11-03 17:55:17 +0100

    add

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,44 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const btn = document.querySelector("div>button");
+const boxes = document.querySelector("div#boxes");
+const number = document.querySelector("input");
+
+const create = (event) => {
+  for (let i = 0; i < number.value; i++) {
+    if (i > number.max) {
+      alert("max value = 100");
+      return false;
+    }
+  }
+  for (let i = 0; i < number.value; i++) {
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+    if (boxes.children.length < number.max) {
+      const div = document.createElement("div");
+      const size = boxes.children.length * 10 + 30 + "px";
+      div.style.height = size;
+      div.style.width = size;
+      div.style.backgroundColor = getRandomHexColor();
+      boxes.append(div);
+    }
+    if (boxes.children.length >= number.max) {
+      alert("You have reached the maximum number of boxes = 100!");
+    }
+  }
+};
+
+const destroy = (event) => {
+  for (let i = 0; i < number.value; i++) {
+    if (0 < boxes.children.length) {
+      const div = document.querySelector("div>div");
+      div.remove();
+    }
+    if (0 >= boxes.children.length) {
+      alert("There are no more boxes to destroy");
+      return false;
+    }
+  }
+};
+
+btn.addEventListener("click", create);
+btn.nextElementSibling.addEventListener("click", destroy);
