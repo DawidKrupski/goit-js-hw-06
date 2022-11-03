e838d6bf76ea10b15af3d935f019f3239a5270f4
DawidKrupski <dawid.krupski323@gmail.com>
2022-11-03 14:26:34 +0100

    add

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const body = document.querySelector("body");
+const btn = document.querySelector("button.change-color");
+const color = document.querySelector("span.color");
+
+const changeColor = (event) => {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+};
+console.log();
+
+btn.addEventListener("click", changeColor);
