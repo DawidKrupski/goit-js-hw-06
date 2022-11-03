0ad4c04e7af5085890b748ebcf34faaf90c7c11e
DawidKrupski <dawid.krupski323@gmail.com>
2022-11-03 13:11:36 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector("form");
+const input = document.querySelectorAll("input");
+const submit = document.querySelector("button");
 
+const check = (event) => {
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].value === "") {
+      alert("Complete the form");
+      return false;
+    }
+  }
+  for (let i = 0; i < input.length; i++) {
+    console.log(input[i].name + ":", input[i].value);
+  }
+  form.reset();
+};
+
+submit.addEventListener("click", check);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
